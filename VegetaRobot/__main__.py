64235161d6b85c1989fc6cddeb82e71efdbe313e
hasnainkkk07 <164120246+hasnainkkk07@users.noreply.github.com>
2024-04-23 11:22:15 +0545

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -244,8 +244,7 @@ def start(update: Update, context: CallbackContext):
 
         else:            
              image = random.choice(VEGETA_IMG)
-            update.effective_message.reply_text(
-                PM_START_TEXT.format(image),
+            update.effective_message.reply_text(PM_START_TEXT.format(image),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
